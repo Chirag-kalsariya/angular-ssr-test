c7b81ca56bfbf617df3621ab26e251823395378d
Charles <chiragkalsariya05@gmail.com>
2023-10-14 13:54:22 +0530

    Bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { SubPageOneComponent } from './components/sub-page-one/sub-page-one.component';
+import { SubPageTwoComponent } from './components/sub-page-two/sub-page-two.component';
+import { HomeScreenWrapperComponent } from './components/home-screen-wrapper/home-screen-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenWrapperComponent,
+    HomeScreenComponent,
+    SubPageOneComponent,
+    SubPageTwoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
